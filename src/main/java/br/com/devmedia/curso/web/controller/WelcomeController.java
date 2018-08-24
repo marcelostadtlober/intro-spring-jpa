6fe997305904372768e75da8f624135dccd03baf
Marcelo Stadtlober <marcelostadtlober@gmail.com>
2018-08-24 16:30:48 -0300

    Primeiro commit com a estrutura base do projeto com JPA

---
+++ src/main/java/br/com/devmedia/curso/web/controller/WelcomeController.java
@@ -0,0 +1,14 @@
+package br.com.devmedia.curso.web.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class WelcomeController {
+	
+	@GetMapping("/")
+	public String welcome() {
+		return "redirect:/usuario/todos";
+	}
+
+}
