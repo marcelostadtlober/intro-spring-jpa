2d64bb67f004171498fd0762aec704bd5b132330
Marcelo Stadtlober <marcelostadtlober@gmail.com>
2018-08-24 17:53:25 -0300

    Integrando ao Spring o Hibernate e JPA

--- src/main/java/br/com/devmedia/curso/config/SpringJpaConfig.java
+++ src/main/java/br/com/devmedia/curso/config/SpringJpaConfig.java
@@ -1,5 +1,81 @@
 package br.com.devmedia.curso.config;
 
+import java.util.Properties;
+
+import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+
+/**
+ * 
+ * @author Marcelo
+ * 
+ * Classe de Integração entre o Spring, o Hibernate e a Jpa.
+ *
+ */
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaDialect;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+
+@Configuration
+@EnableTransactionManagement	// Informa ao Spring que ele gerenciará as transações da aplicação
 public class SpringJpaConfig {
+	
+	/*
+	 * Método que configura a conexão com o banco de dados
+	 */
+	@Bean
+	public DataSource dataSource() {
+		DriverManagerDataSource ds = new DriverManagerDataSource();
+		ds.setDriverClassName("com.mysql.jdbc.Driver");
+		ds.setUrl("jdbc:mysql://localhost:3306/test?createDatabaseIfNotExist=true");
+		ds.setUsername("root");
+		ds.setPassword("");
+		
+		return ds;
+	}
+	
+	/*
+	 * Método que cria um EntityManager para a aplicação
+	 */
+	@Bean
+	public EntityManagerFactory entityManagerFactory(DataSource dataSource) {
+		LocalContainerEntityManagerFactoryBean factory = new LocalContainerEntityManagerFactoryBean();
+		factory.setDataSource(dataSource);
+		factory.setPackagesToScan("br.com.devmedia.curso.domain");
+		factory.setJpaVendorAdapter(new HibernateJpaVendorAdapter());
+		factory.setJpaProperties(jpaProperties());
+		factory.afterPropertiesSet();
+		
+		return factory.getObject();
+	}
+
+	@Bean
+	public JpaTransactionManager transactionalManager(EntityManagerFactory factory) {
+		JpaTransactionManager tx = new JpaTransactionManager();
+		tx.setEntityManagerFactory(factory);
+		tx.setJpaDialect(new HibernateJpaDialect());
+		
+		return tx;
+	}
+	
+	/*
+	 * Método que passa as configurações adicionais ao Jpa
+	 */
+	private Properties jpaProperties() {
+		Properties props = new Properties();
+		props.setProperty("hibernate.show_sql", "true");
+		props.setProperty("hibernate.format_sql", "true");
+		props.setProperty("hibernate.hbm2ddl.auto", "update");
+		
+		return props;
+	}
 
 }
